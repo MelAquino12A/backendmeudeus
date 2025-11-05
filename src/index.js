c9227288237a69de81f16895cd339f7b394a3f73
Melissa Aquino <melissa.aquino.andrade@gmail.com>
2025-11-05 00:47:16 -0300

    eu nao aguento mais minhas costas doem eu errei tudo e nn sei concertar hahahhhashdhs

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import { persons } from "./persons.js"
+
+const app = express()
+const port = 3333
+
+app.use(cors())
+app.use(express.json())
+
+// GET. POST, DELETE, PATCH, PUT
+app.get("/", (request, response) => {
+response.json (persons)
+})
+
+app.post("/cadastrar", (request, response) => {
+  const {name, email, age, nickname, password } = request.body.user
+  
+  console.log(`${name}, ${email}, ${age}, ${nickname}, ${password}`)
+
+
+  response.status(201).json({ message: "Usuário cadastrado com sucesso"})
+})
+
+app.listen(port, () => {
+console.log(`Servidor rodando na porta ${port}!`)
+})
